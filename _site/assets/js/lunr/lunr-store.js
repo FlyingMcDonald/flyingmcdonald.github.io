e0e47c5775ee77e4d93adf2e41f4519bcceb11d1
FlyingMcDonald <1476211809@qq.com>
2020-09-08 11:09:58 +0800

    delete _site

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "http://localhost:4000/jekyll/update/2020/09/08/welcome-to-jekyll.html",
-        "teaser": "http://localhost:4000/assets/images/500x300.png"
-      }]
